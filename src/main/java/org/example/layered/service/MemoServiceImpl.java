9f89760ac458a295e6277148793eda91bf70870b
DESKTOP-M7S61C8\박수박 <pospara2@gmail.com>
2025-03-23 23:54:57 +0900

    FEAT : ããJDBC TEMPLATE & SPRING BOOT 3LAYERED PRACTICE

--- src/main/java/org/example/layered/service/MemoServiceImpl.java
+++ src/main/java/org/example/layered/service/MemoServiceImpl.java
@@ -7,9 +7,11 @@ import org.example.layered.repository.MemoRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Annotation @Service는 @Component와 같다, Spring Bean으로 등록한다는 뜻.
@@ -27,15 +29,13 @@ public class MemoServiceImpl implements MemoService {
     }
 
     @Override
-    public MemoResponseDto saveMemo(MemoRequestDto dto) {
+    public MemoResponseDto saveMemo(MemoRequestDto requestDto) {
 
-        // 요청받은 데이터로  MEMO 객체를 생성, 식별자인 ID 값은 없음 --> 식별자는 저장소의 역할
-        Memo memo = new Memo(dto.getTitle(), dto.getContents());
+        // 요청받은 데이터로 Memo 객체 생성
+        Memo memo = new Memo(requestDto.getTitle(), requestDto.getContents());
 
-        // DB에 저장 ID가 없는상태로 저장
-        Memo savedMemo = memoRepository.saveMemo(memo);
-
-        return new MemoResponseDto(savedMemo);
+        // 저장
+        return memoRepository.saveMemo(memo);
     }
 
 
@@ -51,29 +51,19 @@ public class MemoServiceImpl implements MemoService {
     @Override
     public MemoResponseDto findMemoById(Long id) {
         // 식별자의 Memo가 없다면?
-        Memo memo = memoRepository.findMemoById(id);
-
-        // 아래 코드 사용 불가.
-        // if (memo == null) {
-        //     return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        // }
-
-        if (memo == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Dose not exist id = " + id);
-        }
-
-        return new MemoResponseDto(memo);
-    }
-
-    @Override
-    public MemoResponseDto updateMemo(Long id, String title, String contents) {
-        // memo 조회
-        Memo memo = memoRepository.findMemoById(id);
+        Optional<Memo> optionalMemo = memoRepository.findMemoById(id);
 
         // NPE 방지
-        if (memo == null) {
+        if (optionalMemo.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
         }
+
+        return new MemoResponseDto(optionalMemo.get());
+    }
+
+    @Transactional
+    @Override
+    public MemoResponseDto updateMemo(Long id, String title, String contents) {
 
         // 필수값 검증
         if (title == null || contents == null) {
@@ -81,42 +71,44 @@ public class MemoServiceImpl implements MemoService {
         }
 
         // memo 수정
-        // ???????? 객체만 수정한다고해서 저장소에서 값이 바뀌다니?? 에?? 저자옷 들어가서 put 안해?
-        memo.update(title, contents);
+        int updatedRow = memoRepository.updateMemo(id, title, contents);
+        // 수정된 row가 0개라면
+        if (updatedRow == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No data has been modified.");
+        }
 
-        return new MemoResponseDto(memo);
+        // 수정된 메모 조회
+        return new MemoResponseDto(memoRepository.findMemoById(id).get());
     }
 
+    @Transactional
     @Override
     public MemoResponseDto updateTitle(Long id, String title, String contents) {
-        // memo 조회
-        Memo memo = memoRepository.findMemoById(id);
 
-        // NPE 방지
-        if (memo == null) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
-        }
         // 필수값 검증
         if (title == null || contents != null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The title is a required value.");
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The title and content are required values.");
         }
 
-        memo.updateTitle(title);
+        // memo 제목 수정
+        int updatedRow = memoRepository.updateTitle(id, title);
+        // 수정된 row가 0개 라면
+        if (updatedRow == 0) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No data has been modified.");
+        }
 
-        return new MemoResponseDto(memo);
+        // 수정된 메모 조회
+        return new MemoResponseDto(memoRepository.findMemoById(id).get());
     }
 
     @Override
     public void deleteMemo(Long id) {
-        // memo 조회 -- 존재하는지 먼저 확인
-        Memo memo = memoRepository.findMemoById(id);
-
-        // NPE 방지
-        if (memo == null) {
+        // memo 삭제
+        int deletedRow = memoRepository.deleteMemo(id);
+        // 삭제된 row가 0개 라면
+        if (deletedRow == 0) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id);
         }
-
-        memoRepository.deleteMemo(id);
 
     }
 }
